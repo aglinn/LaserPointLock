1519f8f16239de8046374c85d657b73ff1d5586e
Matthew Teta <matthewteta@Matthews-MacBook-Pro.local>
2020-08-26 22:04:44 -0600

    Working to separate GUI and logic code. Putting initial changes in the 'Cleaned' directory

--- Cleaned/driver.py
+++ Cleaned/driver.py
@@ -1,14 +1,29 @@
-if __name__ == "__main__":
-    import sys
-    import visa
-    import time
-    import numpy as np
-    import pyqtgraph as pg
-    from pointing_ui import Ui_MainWindow
-    from PyQt5 import QtCore, QtGui, QtWidgets,QtSvg
-    from camera import MightexCamera, MightexEngine, DeviceNotFoundError, FakeCamera
-    from motors import MDT693A_Motor, FakeMotor
-    import tkinter as tk
-    from tkinter import filedialog
+"""
+Created by Matthew Teta
 
-    print("test")
+This is the main executable file. 
+This file will instantiate CameraManager, MotorManager, and App
+Passing the CameraManager and MotorManager instances into App will allow everything to communicate
+"""
+
+import sys
+import time
+import numpy as np
+import visa
+from PyQt5.QtCore import QStateMachine, QTimer
+
+# local files
+# from camera import MightexCamera, MightexEngine, DeviceNotFoundError, FakeCamera
+from camera import FakeCamera
+from motors import MDT693A_Motor, FakeMotor
+
+from App import App
+
+def mainExec():
+    print("Attempting to create a window")
+    app = App()
+
+
+
+if __name__ == "__main__":
+    mainExec()
