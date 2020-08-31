10adaf9fd37ac22305b67bfb2580e5739ae0e282
Matthew Teta <matthewtetadev@gmail.com>
2020-08-31 15:32:00 -0600

    Rewrote motors to subclass Motor and wrote container class MotorManager. This breaks things for now

--- Packages/motors.py
+++ Packages/motors.py
@@ -2,195 +2,254 @@ import visa
 from pyvisa.constants import StopBits, Parity, VI_ASRL_FLOW_NONE
 from typing import NewType
 import re
+import numpy
 
-VisaResourceManager = NewType('VisaResourceManager', visa.ResourceManager)
+class MotorManager():
+    resourceManager: visa.ResourceManager = None
 
-class FakeMotor():
-    def __init__(self, ch1: str, ch2: str):
-        self._ch1 = ch1
-        self._ch2 = ch2
-        self._count = 2  # the motor will require multiple sets to reach its target
-        self._ch1_v = 0
-        self._ch2_v = 0
+    def __init__(self):
+        # Create resource manager to talk to devices
+        self.resourceManager = visa.resourceManager()
+
+        self.portList = []
+        self.motorList = []
+
+    # returns list of all connected devices
+    def getDevices(self):
+        return self.resourceManager.list_resources()
     
-    @property
-    def ch1(self):
-        return self._ch1
+    def getMotorList(self):
+        return self.motorList
 
-    @property
-    def ch2(self):
-        return self._ch2
+    def getMotor(self, index):
+        return self.motorList[index]
 
-    @ch1.setter
-    def ch1(self, val: str):
-        self._ch1 = val
-    
-    @ch2.setter
-    def ch2(self, val: str):
-        self._ch2 = val
-
-    @property
-    def ch1_v(self):
-        if self._count <= 0:
-            self._count = 2
-            # print('Read', self._ch1_v, 'on ch 1')
-            return self._ch1_v
+    # Returns None if port is already in use
+    def addMotor(self, port: str, kind: str):
+        if self.portList.index(port):
+            return None
+        
+        m: Motor = None
+        if kind == "MDT693AMotor":
+            # Open port to communicate with motor
+            mInst = self.resourceManager.open_resource(port, baud_rate=MDT693AMotor.baud_rate, data_bits=MDT693AMotor.data_bits, parity=MDT693AMotor.parity, flow_control=MDT693AMotor.flow_control, stop_bits=MDT693AMotor.stop_bits)
+            m = MDT693AMotor(mInst)
+            self.portList.append(port)
         else:
-            self._count -= 1
-            # print('Read', self._ch1_v / (self._count + 1), 'on ch 1')
-            return self._ch1_v / (self._count + 1)
-    
-    @property
-    def ch2_v(self):
-        if self._count <= 0:
-            self._count = 2
-            # print('Read', self._ch2_v, 'on ch 2')
-            return self._ch2_v
+            m = FakeMotor()
+
+        self.motorList.append(m)
+        return m
+
+    def removeMotor(self, index):
+        if index >= 0 and index < len(self.motorList):
+            m = self.motorList[index]
+            self.motorList.remove(m)
+            if m.kind == "MDT693AMotor":
+                m.inst.close()
+
+
+class Motor():
+    kind = "Motor"
+
+    def getVoltageRange(self):
+        return [0.0, 100.0]
+
+    def getXVoltage(self):
+        pass
+
+    def getYVoltage(self):
+        pass
+
+    def getVoltages(self):
+        pass
+
+    def setXVoltage(self, v: float):
+        pass
+
+    def setYVoltage(self, v: float):
+        pass
+
+    def setVoltages(self, v: tuple):
+        pass
+
+
+class FakeMotor(Motor):
+    kind = "FakeMotor"
+    voltageRange = (0.0, 100.0)
+
+    def __init__(self):
+        self.xv = 0 # X Voltage
+        self.yv = 0 # Y Voltage
+        self.xcount = 2
+        self.ycount = 2
+
+    def getVoltageRange(self):
+        return self.voltageRange
+
+    def getXVoltage(self):
+        r = self.xv / self.count
+        self.xcount -= 1
+        if self.xcount == 0:
+            self.xcount = 2
+        return r
+
+    def getYVoltage(self):
+        r = self.yv / self.count
+        self.ycount -= 1
+        if self.ycount == 0:
+            self.ycount = 2
+        return r
+
+    def getVoltages(self):
+        return (self.getXVoltage(), self.getYVoltage())
+
+    def setXVoltage(self, v: float):
+        if v < self.voltageRange[0]:
+            self.xv = self.voltageRange[0]
+        elif v > self.voltageRange[1]:
+            self.xv = self.voltageRange[1]
         else:
-            self._count -= 1
-            # print('Read', self._ch2_v / (self._count + 1), 'on ch 2')
-            return self._ch2_v / (self._count + 1)
+            self.xv = v
+        return self.xv
 
-    @ch1_v.setter
-    def ch1_v(self, v: float):
-        # print('Setting ch 1 voltage to', v)
-        self._ch1_v = v
+    def setYVoltage(self, v: float):
+        if v < self.voltageRange[0]:
+            self.yv = self.voltageRange[0]
+        elif v > self.voltageRange[1]:
+            self.yv = self.voltageRange[1]
+        else:
+            self.yv = v
+        return self.yv
 
-    @ch2_v.setter
-    def ch2_v(self, v: float):
-        # print('Setting ch 2 voltage to', v)
-        self._ch2_v = v
+    def setVoltages(self, v: tuple):
+        pass
 
-class MDT693A_Motor():
+class MDT693AMotor(Motor):
     """
     Code that implements a Motor assuming a connection to the ThorLabs MDT693A
+
+    x_id should be a string that helps direct the motor to control the software X
+    y_id should be a string that helps direct the motor to control the software Y
+
+    # Baud Rate: 115200
+    # Date bits: 8
+    # Parity: none
+    # Stop Bits: 1
+    # Flow control: none
     """
-    def __init__(self, rm: VisaResourceManager, com_port: str, ch1: str, ch2: str):
-        """
-        rm is a pyvisa ResourceManager
-        com_port is the RS232 com port for the motor
-        ch1 should be a string that helps direct the motor to control the software X
-        ch2 should be a string that helps direct the motor to control the software Y
-        """
-        # Baud Rate: 115200
-        # Date bits: 8
-        # Parity: none
-        # Stop Bits: 1
-        # Flow control: none
-        self.port = com_port
-        self.inst = rm.open_resource(com_port, baud_rate=115200, data_bits=8, parity=Parity.none, flow_control=VI_ASRL_FLOW_NONE, stop_bits=StopBits.one)
-        self._ch1 = ch1
-        self._ch2 = ch2
+
+    xId = 'X'
+    yId = 'Y'
+
+    baud_rate=115200
+    data_bits=8
+    parity=Parity.none
+    flow_control=VI_ASRL_FLOW_NONE
+    stop_bits=StopBits.one
+
+    voltageRange = (0.0, 100.0)
+
+    def __init__(self, resourceInst):
+        super().__init__()
+        
+        self.inst = resourceInst
         self.inst.query('XV75.0')
         self.inst.query('YV75.0')
-        self._ch1_v = 75.0
-        self._ch2_v = 75.0
+        self.xv = 75.0
+        self.yv = 75.0
 
-    def terminate(self):
-        # Close connection to port
-        pass
-
-    def change_port(self, val):
-        # Close connection to port
-        # Open connection to new port
-        pass
-
-    @property
-    def ch1(self):
-        return self._ch1
-
-    @property
-    def ch2(self):
-        return self._ch2
-
-    @ch1.setter
-    def ch1(self, val: str):
-        self._ch1 = val
-    
-    @ch2.setter
-    def ch2(self, val: str):
-        self._ch2 = val
-
-    @property
-    def ch1_v(self):
-        cmd = self.ch1 + 'R?'
-        TryAgain=True
-        count = 0
-        while TryAgain == True:
-            rep = re.findall("\d+\.\d+", self.inst.query(cmd))
-            # Return the last setpoint of the voltage, unless the readout is more than 0.3V away, because I found that the readout can be wrong by 0.2
+    def getXVoltage(self):
+        cmd = f'{self.xID} R?'
+        while attempt in range(10): # Try to get value at most 10 times before raising an error
             try:
-                if abs(float(rep[0])-self._ch1_v)>0.3:
-                    return float(rep[0])
-                else:
-                    return self._ch1_v
-                TryAgain = False
-            except IndexError:
-                count += 1
-            if count==10:
-                print("Shit, never could read the ch1_v, giving up")
-                TryAgain = False
-                return self._ch1_v
+                reading = float(re.findall("\d+\.\d+", self.inst.query(cmd))[0])
+                # Return the last setpoint of the voltage, unless the readout is more than 0.3V away,
+                # because I found that the readout can be wrong by 0.2
+                if reading:
+                    if abs(reading - self.xv) > 0.3:
+                        return self.xv
+                    return reading
+            except IndexError: # No match regex
+                print("Trying to read motor voltage again.")
+        print("Could not read motor voltage")
+        return None
 
-    @property
-    def ch2_v(self):
-        cmd = self.ch2 + 'R?'
-        #Return the last setpoint of the voltage, unless the readout is more than 0.3V away, because I found that the readout can be wrong by 0.2
-        TryAgain = True
-        count = 0
-        while TryAgain == True:
-            rep = re.findall("\d+\.\d+", self.inst.query(cmd))
-            # Return the last setpoint of the voltage, unless the readout is more than 0.3V away, because I found that the readout can be wrong by 0.2
+    def getYVoltage(self):
+        cmd = f'{self.yID} R?'
+        while attempt in range(10): # Try to get value at most 10 times before raising an error
             try:
-                if abs(float(rep[0]) - self._ch2_v) > 0.3:
-                    return float(rep[0])
-                else:
-                    return self._ch2_v
-                TryAgain = False
+                reading = float(re.findall("\d+\.\d+", self.inst.query(cmd))[0])
+                # Return the last setpoint of the voltage, unless the readout is more than 0.3V away,
+                # because I found that the readout can be wrong by 0.2
+                if reading:
+                    if abs(reading - self.yv) > 0.3:
+                        return self.yv
+                    return reading
+            except IndexError: # No match regex
+                print("Trying to read motor voltage again.")
+        print("Could not read motor voltage")
+        return None
+
+    def getVoltages(self):
+        return (self.getXVoltage(), self.getYVoltage())
+
+    def setXVoltage(self, v: float):
+        if v < self.voltageRange[0]:
+            self.xv = self.voltageRange[0]
+        elif v > self.voltageRange[1]:
+            self.xv = self.voltageRange[1]
+        else:
+            self.xv = v
+
+        cmd = f'{self.xId}V' + str('{:.1f}'.format(self.xv))
+        while attempt in range(10):
+            try:
+                # Set voltage by sending cmd
+                rep = self.inst.query(cmd)
+                # Extract set Voltage from the full returned string
+                reading = self.getXVoltage()
+                if reading:
+                    if abs(reading - self.xv) > 0.5:
+                        raise NotImplementedError('Could not set X voltage correctly.')
+                    else:
+                        return reading
             except IndexError:
-                count += 1
-            if count == 10:
-                print("Shit, never could read the ch2_v, giving up")
-                TryAgain = False
-                return self._ch2_v
+                print("Trying to read motor voltage again.")
+        print("Could not read motor voltage, X motor voltage is likely set incorrectly")
+        return None
 
-    @ch1_v.setter
-    def ch1_v(self, v: float):
-        self._ch1_v = v
-        #cmd = self.ch1 + 'V ' + '{:.1f}'.format(v)
-        cmd = self.ch1 + 'V' + str('{:.1f}'.format(v))
-        TryAgain = True
-        count = 0
-        while TryAgain == True:
-            rep = self.inst.query(cmd)
-            rep = re.findall("\d+\.\d+", self.inst.query(cmd)) # Extract set Voltage from the full returned string
-            if abs(float('{:.1f}'.format(v))-float(rep[0]))>0.5:
-                count += 1
-                if count > 10:
-                    raise NotImplementedError('Shit the Ch1 Voltage was never set correctly.')
-            else:
-                TryAgain = False
-        return rep[0]
+    def setYVoltage(self, v: float):
+        if v < self.voltageRange[0]:
+            self.yv = self.voltageRange[0]
+        elif v > self.voltageRange[1]:
+            self.yv = self.voltageRange[1]
+        else:
+            self.yv = v
 
-    @ch2_v.setter
-    def ch2_v(self, v: float):
-        self._ch2_v = v
-        #cmd = self.ch2 + 'V ' + '{:.1f}'.format(v)
-        cmd = self.ch2 + 'V' + str('{:.1f}'.format(v))
-        TryAgain = True
-        count = 0
-        while TryAgain == True:
-            rep = self.inst.query(cmd)
-            rep = re.findall("\d+\.\d+", self.inst.query(cmd))  # Extract set Voltage from the full returned string
-            if abs(float('{:.1f}'.format(v)) - float(rep[0])) > 0.5:
-                count += 1
-                if count > 10:
-                    raise NotImplementedError('Shit the Ch2 Voltage was never set correctly.')
-            else:
-                TryAgain = False
-        return rep[0]
+        cmd = f'{self.yId}V' + str('{:.1f}'.format(self.yv))
+        while attempt in range(10):
+            try:
+                # Set voltage by sending cmd
+                rep = self.inst.query(cmd)
+                # Extract set Voltage from the full returned string
+                reading = self.getXVoltage()
+                if reading:
+                    if abs(reading - self.yv) > 0.5:
+                        raise NotImplementedError('Could not set Y voltage correctly.')
+                    else:
+                        return reading
+            except IndexError:
+                print("Trying to read motor voltage again.")
+        print("Could not read motor voltage, Y motor voltage is likely set incorrectly")
+
+        return None
+
+    def setVoltages(self, v: tuple):
+        self.setXVoltage(v[0])
+        self.setYVoltage(v[1])
     
-    def get_info(self):
+    def getInfo(self):
         cmd = 'I'
         rep = self.inst.query(cmd)
         return rep
