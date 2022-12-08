c1edbe94fb13cba8ef50ac84b4ab2d4672535194
aglinn <Garrison.Linn@colorado.edu>
2022-12-08 16:02:53 -0700

    .

--- display_new.py
+++ display_new.py
@@ -286,7 +286,8 @@ class Window(QMainWindow, Ui_MainWindow):
     @pyqtSlot(float)
     def report_UpdateManager_ping(self, ping_time: float):
         if ping_time > 0.005:
-            print(ping_time, " is how long the Update Manager took to receive a signal to enter its slot.")
+            pass
+            # print(ping_time, " is how long the Update Manager took to receive a signal to enter its slot.")
         return
 
     def connectSignalsSlots(self):
